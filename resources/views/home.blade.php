d261be3b47c53951f04d228e3d46a47fe39960a1
Joe Smith <jsmitty54@verizon.net>
2015-10-25 14:56:59 -0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('title')
+    Joe's Music Library
+@stop
+
+
+{{--
+This `head` section will be yielded right before the closing </head> tag.
+Use it to add specific things that *this* View needs in the head,
+such as a page specific styesheets.
+--}}
+@section('head')
+
+@stop
+
+
+
+@section('content')
+<br>
+<p>Joe's Music Library Application</p>
+<hr>
+
+@stop
+
+
+{{--
+This `body` section will be yielded right before the closing </body> tag.
+Use it to add specific things that *this* View needs at the end of the body,
+such as a page specific JavaScript files.
+--}}
+@section('body')
+
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        {{-- Yield the title if it exists, otherwise default to 'Foobooks' --}}
+        @yield('title','Joes Toolbox')
+    </title>
+
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+    <link href="/css/jmscss.css" type='text/css' rel='stylesheet'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+    @yield('head')
+
+</head>
+<body>
+  <div id="wrapper" class="container-fluid">
+
+    <div id="header" class="row">
+      <div id="topHeader" class="">
+
+      	<div class="transbox">
+      		@yield('title','Joes Toolbox')
+        </div>
+
+      </div>
+    </div>
+    <div id="maincontent" class="row">
+
+        <div id="leftgutter" class="col-sm-3">
+          <h2>Menu</h2>
+            <ul class="nav nav-stacked">
+
+              <li><a class="" href="http://p1.jsmitty54php.com">Back to Portfolio Page</a></li>
+              
+            </ul>
+        </div>
+
+
+        <div id="center" class="col-sm-9">
+          {{-- Main page content will be yielded here --}}
+          @yield('content')
+        </div>
+
+      </div>
+
+      <div id="footer" class="row">
+        Dynamic Web Applications&nbsp;*&nbsp;Fall 2015&nbsp;*&nbsp;Instructor: Susan Buck&nbsp;*&nbsp;Author / Student: Joe Smith
+      </div>
+
+
+
+
+    </div>
+  {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+  @yield('body')
+
+</body>
+</html>
